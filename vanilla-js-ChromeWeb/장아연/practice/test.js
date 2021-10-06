62d9ce1765cd828e5eb49246f59bdf4d416e7324
JangAyeon <>
2021-10-06 12:17:12 +0900

    [GDSC FE Study {1}] 장아연 learn get/save/load input

--- vanilla-js-ChromeWeb/장아연/practice/test.js
+++ vanilla-js-ChromeWeb/장아연/practice/test.js
@@ -1,54 +1,39 @@
-const API_KEY="cb885eb10dc47392d636edbc7b0c5380";
-const weather=document.querySelectorAll("#weather span");
+const loginForm=document.querySelector("#login-form");
+const loginInput=document.querySelector("#login-form input");
+const greeting=document.getElementById("#greeting");
 
+const HIDDEN_CLASSNAME="hidden";
+const USERNAME_KEY="username";
 
-function onGeoOk(position){
-  const lat=position.coords.latitude;
-  const lon=position.coords.longitude; 
-  const url_weather=`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  const url_air=`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  let air_quality=""
-
-  fetch(url_weather).then(response=>response.json()).then(data=>{
-    weather[0].innerText=`${data.weather[0].main} / ${data.main.temp} °C`;
-    weather[2].innerText="@"+data.name;
-  });
-
-  fetch(url_air).then(response=>response.json()).then(data=>{
-    const air_index=data.list[0].main.aqi;
-    
-
-    switch (air_index) {
-      case 1:
-        air_quality = "Good";
-        break;
-
-      case 2:
-        air_quality ="Fair";
-        break;
-    
-      case 3:
-        air_quality = "Moderate";
-        break;
-    
-      case 4:
-        air_quality = "Poor";
-        break;
-    
-      case 5:
-        air_quality = "Very Poor";    
-    }
-    weather[1].innerText="Air : "+air_quality;
-
-  });
+function onLoginSubmit(event){
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username=loginInput.value;
+  localStorage.setItem("USERNAME_KEY",username);
+  paintGreetings(username);
 
 
 
 }
 
-function onGeoError(){
-  alert("Can't find you. No weather for you");
+
+function paintGreetings(username){
+  greeting.innerText=`${username}\n daily`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
 
+
+const savedUsername=localStorage.getItem("USERNAME_KEY");
+if(savedUsername===null){
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit",onLoginSubmit);
+}
+else{
+  //show the greetings
+  paintGreetings(savedUsername);
+  
+
+
+}
