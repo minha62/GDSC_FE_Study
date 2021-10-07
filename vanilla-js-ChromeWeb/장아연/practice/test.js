50ee5748be501c342a9812dd1e2d7244f65a7bf6
JangAyeon <>
2021-10-07 12:15:59 +0900

    [GDSC FE Study {1}] 장아연 learn add/load/remove/check todos

--- vanilla-js-ChromeWeb/장아연/practice/test.js
+++ vanilla-js-ChromeWeb/장아연/practice/test.js
@@ -1,39 +1,87 @@
-const loginForm=document.querySelector("#login-form");
-const loginInput=document.querySelector("#login-form input");
-const greeting=document.getElementById("#greeting");
+const toDoForm=document.getElementById("todo-form");
+const toDoInput=document.querySelector("#todo-form input");
+const ToDoList=document.getElementById("todo-list");
+const TODOS_KEY="todos";
+const TODOS_DONE_KEY="todo_done";
+let ToDos=[]; /*start with empty todo elements */
+let ToDos_Done=[];
 
-const HIDDEN_CLASSNAME="hidden";
-const USERNAME_KEY="username";
-
-function onLoginSubmit(event){
-  event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username=loginInput.value;
-  localStorage.setItem("USERNAME_KEY",username);
-  paintGreetings(username);
-
-
-
-}
-
-
-function paintGreetings(username){
-  greeting.innerText=`${username}\n daily`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-}
-
-
-
-const savedUsername=localStorage.getItem("USERNAME_KEY");
-if(savedUsername===null){
-  //show the form
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit",onLoginSubmit);
-}
-else{
-  //show the greetings
-  paintGreetings(savedUsername);
+function saveToDos(){
   
+  localStorage.setItem("TODOS_KEY",JSON.stringify(ToDos)); 
+  /*save todo element in localstorage as text (formed like object)*/
+}
+
+
+function deleteTodo(event){
+  const delete_li=event.target.parentElement; /*get li component from event that user's delete click*/
+  delete_li.remove();
+  ToDos=ToDos.filter(ToDo=>ToDo.id!=parseInt(delete_li.id));
+  saveToDos();
+  
+}
+
+function getChecked(check_li){
+  const yet_checked=document.getElementById(check_li);
+  yet_checked.classList.add("check")
+}
+
+function checkTodo(event){
+  const check_li=event.target.parentElement;
+  check_li.classList.add("check");
+  ToDos_Done.push(check_li.id);
+  localStorage.setItem("TODOS_DONE_KEY",JSON.stringify(ToDos_Done)); 
 
 
 }
+
+function paintToDo(newTodoObj){
+ const todo_li=document.createElement("li"); /*create li HTML tag*/
+ todo_li.id=newTodoObj.id;/* li tag attribution id is same with todo id*/
+ const todo_span=document.createElement("span"); /*create span HTML tag */
+ todo_span.innerText=newTodoObj.text; /*show newTodo value in span*/
+
+ const todo_delete=document.createElement("button"); /*create button HTML tag*/
+ todo_delete.innerText="❌"
+ todo_delete.addEventListener("click",deleteTodo);
+ const todo_check=document.createElement("button"); /*create button HTML tag*/
+ todo_check.innerText="⭕";
+ todo_check.addEventListener("click",checkTodo);
+
+ todo_li.appendChild(todo_span); /*make span tag go inside of li tag*/
+ todo_li.appendChild(todo_delete); /*make delete button tag go inside of li tag*/
+ todo_li.appendChild(todo_check);/*make check button tag go inside of li tag*/
+ ToDoList.appendChild(todo_li);
+
+}
+
+
+function handleTodoSubmit(event){
+  event.preventDefault();
+  const newTodo=toDoInput.value; /* get input value*/
+  toDoInput.value=""; /*make input form empty after submit*/
+  const newTodoObj={ /*create todo obj {id : todo}*/
+    text:newTodo,
+    id:Date.now()
+  }
+  ToDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit",handleTodoSubmit);
+
+/*load todo element*/
+const savedToDos=localStorage.getItem("TODOS_KEY");
+const doneToDos=localStorage.getItem("TODOS_DONE_KEY");
+
+if (savedToDos!=null){ /*if todo already exist, before you enter*/
+  const parsedToDos=JSON.parse(savedToDos);
+  ToDos=parsedToDos; /*get already entered todo element*/
+  parsedToDos.forEach(paintToDo); /*display*/
+}
+
+if (doneToDos!=null){ /*if todo already exist, before you enter*/
+  const parsed_doneToDos=JSON.parse(doneToDos);
+  ToDos_Done=parsed_doneToDos; /*get already entered todo element*/
+  parsed_doneToDos.forEach(getChecked)}
