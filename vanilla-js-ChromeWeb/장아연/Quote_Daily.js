e039065f04127054953170442c49bd9331ece913
JangAyeon <>
2021-10-07 09:54:12 +0900

    [GDSC FE Study {1}] 장아연 add username input in daily report

--- vanilla-js-ChromeWeb/장아연/Quote_Daily.js
+++ vanilla-js-ChromeWeb/장아연/Quote_Daily.js
@@ -1,3 +1,50 @@
+/*Daily report*/
+
+
+const loginForm=document.querySelector("#login-form");
+const loginInput=document.querySelector("#login-form input");
+const greeting=document.getElementById("greeting")
+
+
+const HIDDEN_CLASSNAME="hidden";
+const USERNAME_KEY="username";
+
+function onLoginSubmit(event){
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username=loginInput.value;
+  localStorage.setItem("USERNAME_KEY",username);
+  paintGreetings(username);
+
+
+
+}
+
+
+function paintGreetings(username){
+  greeting.innerText=`${username}\n Daily \nReport`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+
+const savedUsername=localStorage.getItem("USERNAME_KEY");
+if(savedUsername===null){
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit",onLoginSubmit);
+}
+else{
+  //show the greetings
+  paintGreetings(savedUsername);
+  
+
+
+}
+
+
+/* Daily Quote*/
+
 const quotes = [
     {
       quote: "The way to get started is to quit talking and begin doing.",
