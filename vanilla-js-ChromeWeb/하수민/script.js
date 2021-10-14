5504bbba51581164fe729e729414a1734aa832be
Sumin Ha <71062967+minha62@users.noreply.github.com>
2021-10-14 19:35:37 +0900

    toggle

--- vanilla-js-ChromeWeb/하수민/script.js
+++ vanilla-js-ChromeWeb/하수민/script.js
@@ -1,14 +1,15 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-
-    if(currentColor === "blue"){
-        newColor = "tomato";
+    /**
+    const clickedClass = "clicked"
+    if(h1.classList.contains(clickedClass)){  // 초기의 클래스네임 계속 남아있음
+        h1.classList.remove(clickedClass);
     } else{
-        h1.className = clickedClass; //최초의 클래스네임은 사라짐
+        h1.classList.add(clickedClass);
     }
+    */
+    h1.classList.toggle("clicked"); //위으 코드를 toggle 사용해서 한 줄로 만들 수 있음
 }
 
 h1.addEventListener("click", handleTitleClick);
