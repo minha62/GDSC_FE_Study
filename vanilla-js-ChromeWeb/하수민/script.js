b8ce2fe65853f159ffa14aa37647329a0021fdd4
Sumin Ha <71062967+minha62@users.noreply.github.com>
2021-10-14 18:01:01 +0900

    Event-if/else in JS

--- vanilla-js-ChromeWeb/하수민/script.js
+++ vanilla-js-ChromeWeb/하수민/script.js
@@ -1,41 +1,17 @@
-const title = document.querySelector("div.hello:first-child h1");
-
-console.dir(title);
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    title.style.color = "blue";
+    const currentColor = h1.style.color;
+    let newColor;
+
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    } else{
+        newColor = "blue";
+    }
+    
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){
-    title.innerText = "Mouse is here!";
-}
+h1.addEventListener("click", handleTitleClick);
 
-function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOOOOD");
-}
-
-title.addEventListener("click", handleTitleClick);
-//title.ondlick = handleTitleClick; 똑같지만 addEventListener 사용하면 나중에 remove가능하다는 장점.
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
