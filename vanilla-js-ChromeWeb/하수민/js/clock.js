e63c80809dd43bca61e97eb96e88ea90f9c2cd02
Sumin Ha <71062967+minha62@users.noreply.github.com>
2021-10-29 16:07:26 +0900

    clock - 2자리수로 표현 안됨

--- vanilla-js-ChromeWeb/하수민/js/clock.js
+++ vanilla-js-ChromeWeb/하수민/js/clock.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();
+    clock.innerText= `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000); // 5 seconds
+getClock(); // 먼저 getClock() 호출해주지 않으면 1초동안은 초기값인 00:00:00로 표시
+setInterval(getClock, 1000); 
