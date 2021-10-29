65ef49cd94dff69f3ccffcd3c54c6bff9a621db0
Sumin Ha <71062967+minha62@users.noreply.github.com>
2021-10-29 17:00:13 +0900

    random quotes

---
+++ vanilla-js-ChromeWeb/하수민/js/quotes.js
@@ -0,0 +1,50 @@
+const quotes = [
+    {
+        quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        author: "Albert Einstein",
+    },
+    {
+        quote: "You only live once, but if you do it right, once is enough.",
+        author: "Mae West",
+    },
+    {
+        quote: "Be the change that you wish to see in the world.",
+        author: "Mahatma Gandhi",
+    },
+    {
+        quote: "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+        author: "Maya Angelou",
+    },
+    {
+        quote: "It is better to be hated for what you are than to be loved for what you are not.",
+        author: "Andre Gide, Autumn Leaves",
+    },
+    {
+        quote: "It is our choices, Harry, that show what we truly are, far more than our abilities.",
+        author: "J.K. Rowling",
+    },
+    {
+        quote: "I have not failed. I've just found 10,000 ways that won't work.",
+        author: "Thomas A. Edison",
+    },
+    {
+        quote: "I may not have gone where I intended to go, but I think I have ended up where I needed to be.",
+        author: "douglas adams",
+    },
+    {
+        quote: "That which does not kill us makes us stronger.",
+        author: "Friedrich Nietzsche",
+    },
+    {
+        quote: "Being deeply loved by someone gives you strength, while loving someone deeply gives you courage.",
+        author: "Lao Tzu",
+    },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
