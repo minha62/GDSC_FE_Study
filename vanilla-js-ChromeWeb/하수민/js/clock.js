62820d20b60ffe9c55745d98fcb6a820069d8f7c
Sumin Ha <71062967+minha62@users.noreply.github.com>
2021-10-29 16:12:00 +0900

    padStart - clock 두자리수 표현

--- vanilla-js-ChromeWeb/하수민/js/clock.js
+++ vanilla-js-ChromeWeb/하수민/js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText= `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText= `${hours}:${minutes}:${seconds}`;
 }
 
 getClock(); // 먼저 getClock() 호출해주지 않으면 1초동안은 초기값인 00:00:00로 표시
