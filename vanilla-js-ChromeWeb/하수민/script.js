e178b57a72b368054d694a1fb540160068856a77
Sumin Ha <71062967+minha62@users.noreply.github.com>
2021-10-29 15:18:20 +0900

    localStroage, savedUsername - 존재하면 greeting, 없으면 입력받기

--- vanilla-js-ChromeWeb/하수민/script.js
+++ vanilla-js-ChromeWeb/하수민/script.js
@@ -3,16 +3,29 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting"); // id 앞에 #
 
 const HIDDEN_CLASSNAME = "hidden"; // string만 표함된 변수는 대문자로 표기. string 저장하고 싶을때 사용.
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     const username = loginInput.value;
     event.preventDefault(); // 브라우저기 가본 동작을 하지 못하도록 막음 - 새로고침
     loginForm.classList.add(HIDDEN_CLASSNAME); // username 입력하면 form 사라짐
-    console.log(username);
-    greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`; // 윗줄과 동일. 이 방식 더 선호. ''아니라 백틱 ``
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username){
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else{
+    // show the greetings
+    paintGreetings(savedUsername);
+}
