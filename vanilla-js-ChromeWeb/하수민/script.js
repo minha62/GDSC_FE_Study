c7570f0bdd88c29ed8e2dcafe9b83a098f8172d4
Sumin Ha <71062967+minha62@users.noreply.github.com>
2021-10-29 14:24:38 +0900

    username 입력 -> class hidden, greeting

--- vanilla-js-ChromeWeb/하수민/script.js
+++ vanilla-js-ChromeWeb/하수민/script.js
@@ -1,19 +1,18 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting"); // id 앞에 #
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden"; // string만 표함된 변수는 대문자로 표기. string 저장하고 싶을때 사용.
 
 function onLoginSubmit(event){
-    //const username = loginInput.value;
+    const username = loginInput.value;
     event.preventDefault(); // 브라우저기 가본 동작을 하지 못하도록 막음 - 새로고침
-    console.log(loginInput.value);
-}
-
-function handleLinkClick(event){
-    event.preventDefault();
-    console.dir(event);
-    alert("clicked!"); // 모든 동작 막음. OK 눌러야 동작 실행. 이제 안씀
+    loginForm.classList.add(HIDDEN_CLASSNAME); // username 입력하면 form 사라짐
+    console.log(username);
+    greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`; // 윗줄과 동일. 이 방식 더 선호. ''아니라 백틱 ``
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+
