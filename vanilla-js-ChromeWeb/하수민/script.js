13cd014032bb148234e1a024c283707e1f564169
Sumin Ha <71062967+minha62@users.noreply.github.com>
2021-10-29 14:08:30 +0900

    events - link

--- vanilla-js-ChromeWeb/하수민/script.js
+++ vanilla-js-ChromeWeb/하수민/script.js
@@ -1,12 +1,19 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event){
     //const username = loginInput.value;
     event.preventDefault(); // 브라우저기 가본 동작을 하지 못하도록 막음 - 새로고침
     console.log(loginInput.value);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event);
+    alert("clicked!"); // 모든 동작 막음. OK 눌러야 동작 실행. 이제 안씀
+}
 
-// onLoginSubmit() - 브라우저가 () 보는 순간 바로 실행시켜버림 
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
