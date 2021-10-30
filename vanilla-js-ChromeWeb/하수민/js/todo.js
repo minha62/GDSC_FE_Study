b3afc3dfe16e94e945a2c16f93d1bd8211cd85a5
Sumin Ha <71062967+minha62@users.noreply.github.com>
2021-10-31 01:59:12 +0900

    Saving todo

--- vanilla-js-ChromeWeb/하수민/js/todo.js
+++ vanilla-js-ChromeWeb/하수민/js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("todos", JSON.stringify(toDos)); // JSON.stringify() : string으로 convert해줌. 여기선 array 모양으로 toDos 저장하기 위해 쓰임.
+}
+
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
@@ -23,7 +29,9 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
