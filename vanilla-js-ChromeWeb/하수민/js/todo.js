14b1f54bcfbb3d1f66d6c0cf027a6d97edbdde3e
Sumin Ha <71062967+minha62@users.noreply.github.com>
2021-11-03 04:16:16 +0900

    newToDo를 오브젝트로 변경

--- vanilla-js-ChromeWeb/하수민/js/todo.js
+++ vanilla-js-ChromeWeb/하수민/js/todo.js
@@ -17,8 +17,9 @@ function deleteToDo(event){
 
 function paintToDo(newTodo){
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
@@ -31,22 +32,16 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
-
-/*
-아래 둘 다 가능. 
-
-(item) => console.log("this is the turn of ", item);
-
-function sayHello(item){
-    console.log("this is the turn of ", item);    
-}
-*/
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
@@ -55,3 +50,4 @@ if(savedToDos){
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
 }
+
